4d4035e419f436316b4b13cc98b0ba23e0999f2a
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2024-04-11 01:48:15 +0530

    complete code by sanjay

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
-
-
+import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -25,8 +24,10 @@ function App() {
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
-
-
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
